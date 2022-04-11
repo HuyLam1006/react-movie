0c7b920144d31cbc1c119233efdf2c0e629d6968
HuyL4m <huy.lamgia1601@gmail.com>
2022-04-11 19:49:44 +0700

    c

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import NotFoundPage from 'pages/NotFoundPage'
 import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import 'swiper/scss'
-import MainBo from './components/layout/MainBo'
+// import Main from './components/layout/Main'
 // import HomePage from './pages/HomePage'
 // import MovieDetailsPage from './pages/MovieDetailsPage'
 // import MoviePage from './pages/MoviePage'
 
 // dynamic import
-const HomePage = lazy(() => import('./pages/HomePage'))
-const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage'))
-const MoviePage = lazy(() => import('./pages/MoviePage'))
+const Main = lazy(() => import('components/layout/Main'))
+const HomePage = lazy(() => import('pages/HomePage'))
+const MovieDetailsPage = lazy(() => import('pages/MovieDetailsPage'))
+const MoviePage = lazy(() => import('pages/MoviePage'))
 
 function App() {
   return (
     <>
       <Suspense fallback={<></>}>
         <Routes>
-          <Route element={<MainBo />}>
+          <Route element={<Main />}>
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/movie" element={<MoviePage />}></Route>
             <Route
